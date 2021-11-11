99996478ad3b03aee9666661c103bc19b59f8c92
Adel Ansari <adelansari@github.com>
2021-11-11 05:23:59 +0200

    fixing GuildMemberAdd script a bit

--- src/commands/authentication/CaptchaCommand.ts
+++ src/commands/authentication/CaptchaCommand.ts
@@ -1,39 +1,45 @@
-import { Message } from 'discord.js';
-import { MessageAttachment } from 'discord.js';
-import { MessageEmbed } from 'discord.js';
-import BaseCommand from '../../utils/structures/BaseCommand';
-import DiscordClient from '../../client/client';
+import { Message } from "discord.js";
+import { MessageAttachment } from "discord.js";
+import { MessageEmbed } from "discord.js";
+import BaseCommand from "../../utils/structures/BaseCommand";
+import DiscordClient from "../../client/client";
 
-
-import { Captcha } from 'captcha-canvas';  // importing captcha from npm module
-import { MembershipStates } from 'discord.js/typings/enums';
-import GuildMemberAddEvent from '../../events/GuildMemberAddEvent';
-
-
+import { Captcha } from "captcha-canvas"; // importing captcha from npm module
+import { MembershipStates } from "discord.js/typings/enums";
+import GuildMemberAddEvent from "../../events/GuildMemberAddEvent";
 
 export default class CaptchaCommand extends BaseCommand {
   constructor() {
-    super('captcha', 'authentication', []);
+    super("captcha", "authentication", []);
   }
 
   async run(client: DiscordClient, message: Message, args: Array<string>) {
-
     const captcha = new Captcha(); //create a captcha canvas of 100x300.
-    captcha.async = true //Sync
+    captcha.async = true; //Sync
     captcha.addDecoy(); //Add decoy text on captcha canvas.
     captcha.drawTrace(); //draw trace lines on captcha canvas.
     captcha.drawCaptcha(); //draw captcha text on captcha canvas.
 
-    const captchaAttachment = new MessageAttachment(await captcha.png, 'captcha.png');
+    const captchaAttachment = new MessageAttachment(
+      await captcha.png,
+      "captcha.png"
+    );
 
     const captchaEmbed = new MessageEmbed()
-      .setDescription('Please complete this captcha:')
-      .setImage('attachment://captcha.png');
+      .setDescription("Please complete this captcha:")
+      .setImage("attachment://captcha.png");
 
-    const msg = await message.channel.send({ files: [captchaAttachment], embeds: [captchaEmbed] });
+    const msg = await message.channel.send({
+      files: [captchaAttachment],
+      embeds: [captchaEmbed],
+    });
 
+    // const filter = (message: { author: { id: string }; content: string }) => {
+    //   if (message.author.id !== member.id) return;
+    //   if (message.content === captcha.text) return true;
+    //   else member.send("Wrong captcha!");
+    // };
 
     // message.channel.send({ files: [captchaAttachment], content: `Code: ${captcha.text}` });
-
   }
 }
--- src/events/GuildMemberAddEvent.ts
+++ src/events/GuildMemberAddEvent.ts
@@ -1,75 +1,89 @@
 // https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildMemberAdd
-import { GuildMember, TextChannel, Message, ChannelManager, Interaction } from 'discord.js';
-import BaseEvent from '../utils/structures/BaseEvent';
-import DiscordClient from '../client/client';
+import {
+  GuildMember,
+  TextChannel,
+  Message,
+  ChannelManager,
+  Interaction,
+} from "discord.js";
+import BaseEvent from "../utils/structures/BaseEvent";
+import DiscordClient from "../client/client";
 
-import { MessageAttachment, MessageEmbed } from 'discord.js';
-import { Captcha } from 'captcha-canvas';  // importing captcha from npm module
+import { MessageAttachment, MessageEmbed } from "discord.js";
+import { Captcha } from "captcha-canvas"; // importing captcha from npm module
 
 export default class GuildMemberAddEvent extends BaseEvent {
   constructor() {
-    super('guildMemberAdd');
+    super("guildMemberAdd");
   }
 
   async run(client: DiscordClient, member: GuildMember) {
     // console.log(`${member.user.tag} joined to ${member.guild.name}.`);
     // console.log(member.user.tag);
     const captcha = new Captcha(); //create a captcha canvas of 100x300.
-    captcha.async = true //Sync
+    captcha.async = true; //Sync
     captcha.addDecoy(); //Add decoy text on captcha canvas.
     captcha.drawTrace(); //draw trace lines on captcha canvas.
     captcha.drawCaptcha(); //draw captcha text on captcha canvas.
 
-    const captchaAttachment = new MessageAttachment(await captcha.png, 'captcha.png');
+    const captchaAttachment = new MessageAttachment(
+      await captcha.png,
+      "captcha.png"
+    );
 
     const captchaEmbed = new MessageEmbed()
-      .setDescription('Please complete this captcha to get a "**verified**" role:')
-      .setImage('attachment://captcha.png');
+      .setDescription(
+        'Please complete this captcha to get a "**verified**" role:'
+      )
+      .setImage("attachment://captcha.png");
 
+    // // To send the captcha message to 'verfy' channel
+    // const channelId = "906924913984671784";   // verify channel id
+    // const verifychannel = client.channels.cache.get(channelId) as TextChannel  // Finding the "verify" channel
+    // verifychannel.send(`Hi ${member.user}`)
+    // const msg = await verifychannel.send({ files: [captchaAttachment], embeds: [captchaEmbed] });
 
-    const channelId = "906924913984671784";   // verify channel id
-    const verifychannel = client.channels.cache.get(channelId) as TextChannel  // Finding the "verify" channel
-    verifychannel.send(`Hi ${member.user}`)
-    const msg = await verifychannel.send({ files: [captchaAttachment], embeds: [captchaEmbed] });
+    const msg = await member.send({
+      files: [captchaAttachment],
+      embeds: [captchaEmbed],
+    });
 
-    // The following is not working as intended 
-    if (msg.author.id !== member.id) {
-      return;
-    } else if (msg.content === captcha.text) {
-      verifychannel.send('You have been verified!');
-      member.roles.add('906790468023627788');   // racer role id
-    } else {
-      verifychannel.send('Wrong captcha! Please try again using "**.verify**" command');
+    /**
+     * Todo: unable to understand what's happening with a type type casting in this section
+     **/
+
+    const filter = (message: { author: { id: string }; content: string }) => {
+      if (message.author.id !== member.id) {
+        return false;
+      }
+      if (message.content === captcha.text) {
+        console.log(message.content);
+        return true;
+      } else {
+        member.send(
+          'Wrong captcha! Please try again using "**.verify**" command'
+        );
+        return false;
+      }
+    };
+
+    try {
+      const response = await msg.channel.awaitMessages({
+        filter,
+        max: 1, // Number of messages to successfully pass the filter
+        time: 20000, // time in milliseconds
+        errors: ["time"],
+      });
+      if (response) {
+        // when verified
+        await member.roles.add("906790468023627788");
+        await member.send("You have been verified");
+      }
+    } catch (err) {
+      // no time and not verified
+      await member.send("You have not verified.");
+      // await member.send(JSON.stringify(err))
+      console.log(err);
     }
-
-
-
-
-
-
-    // const filter = (message: { author: { id: string; }; content: string; }) => {
-    //   if(message.author.id !== member.id) return;
-    //   if(message.content === captcha.text) return true;
-    //   else member.send("Wrong captcha!")
-    // }
-
-    // try {
-    //   const response = await msg.channel.awaitMessages({
-    //     filter,
-    //     max: 1,
-    //     time: 600000,
-    //     errors: ["time"],
-    //   });
-    //   if(response) {
-    //     // when verified
-    //     member.roles.add('906790468023627788');   // racer role id
-    //   }
-    // } catch (err) {
-    //   // no time and not verified
-    //   await member.send('You have not verified.')
-
-    // }
-
-
   }
 }
