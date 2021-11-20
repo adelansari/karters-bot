5a26c4d952de92dcdd043e8123cd7af7ad3d8b0a
Adel Ansari <adelansari@github.com>
2021-11-20 14:16:38 +0200

    contructor2public + added description array string

--- src/utils/structures/BaseCommand.ts
+++ src/utils/structures/BaseCommand.ts
@@ -3,11 +3,12 @@ import { Message } from 'discord.js';
 import DiscordClient from '../../client/client';
 
 export default abstract class BaseCommand {
-  constructor(private name: string, private category: string, private aliases: Array<string>) {}
+  constructor(public name: string, public category: string, public aliases: Array<string>,  public description: string) {}
 
   getName(): string { return this.name; }
   getCategory(): string { return this.category; }
   getAliases(): Array<string> { return this.aliases; }
+  getDescription(): string { return this.description; }
 
   abstract run(client: DiscordClient, message: Message, args: Array<string> | null): Promise<void>;
 }
