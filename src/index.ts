14b576095f5972c6559ce5e30aff4faf079abf5f
Adel Ansari <adelansari@github.com>
2021-11-22 04:14:01 +0200

    reading token from .env for heroku

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { registerCommands, registerEvents } from "./utils/registry";
 import config from "../slappey.json";
-import botTokenJson from "./botToken.json"; // keep this hidden inside src/botToken.json
+// import botTokenJson from "./botToken.json"; // keep this hidden inside src/botToken.json
 import DiscordClient from "./client/client";
 import { Intents } from "discord.js";
 // const client = new DiscordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });  // Original code
@@ -16,7 +16,8 @@ const client = new DiscordClient({
   client.prefix = config.prefix || client.prefix;
   await registerCommands(client, "../commands");
   await registerEvents(client, "../events");
-  await client.login(botTokenJson.token);
+  // await client.login(botTokenJson.token);
+  await client.login(process.env.TOKEN);
 })();
 
 export interface CMDInterface {
