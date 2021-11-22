d94f023bd80902c75985803335dfc47215260d0d
Adel Ansari <adelansari@github.com>
2021-11-22 05:57:14 +0200

    original index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { registerCommands, registerEvents } from "./utils/registry";
 import config from "../slappey.json";
-// import botTokenJson from "./botToken.json"; // keep this hidden inside src/botToken.json
+import botTokenJson from "./botToken.json"; // keep this hidden inside src/botToken.json
 import DiscordClient from "./client/client";
 import { Intents } from "discord.js";
+
+
 // const client = new DiscordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });  // Original code
 
 // The number 32767 means ALL_INTENTS. There are 15 Intents in total.
@@ -16,8 +18,8 @@ const client = new DiscordClient({
   client.prefix = config.prefix || client.prefix;
   await registerCommands(client, "../commands");
   await registerEvents(client, "../events");
-  // await client.login(botTokenJson.token);
-  await client.login(process.env.TOKEN);
+  await client.login(botTokenJson.token);
+  // await client.login(process.env.TOKEN);
 })();
 
 export interface CMDInterface {
