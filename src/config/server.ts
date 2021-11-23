b8e46a3f98408ef95a402f403a366e8967150c31
Adel Ansari <adelansari@github.com>
2021-11-23 04:25:26 +0200

    removing server file

--- src/config/server.ts
+++
@@ -1,9 +0,0 @@
-// import express from 'express';
-// const app = express()
-// const PORT : string|number = process.env.PORT || 5000;
-
-// app.use("*",(req, res) =>{
-//     res.send("<h1>Welcome to your simple server! Awesome right</h1>");
-// });
-
-// app.listen(PORT,() => console.log(`hosting @${PORT}`));
