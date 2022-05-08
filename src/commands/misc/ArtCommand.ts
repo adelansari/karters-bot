b3e50b45140df78f5133e97ab5abdea7989b3334
Adel Ansari <adelansari@github.com>
2022-05-08 05:45:01 +0300

    Adding pages and buttons

--- src/commands/misc/ArtCommand.ts
+++ src/commands/misc/ArtCommand.ts
@@ -1,25 +1,34 @@
-import { Message, MessageEmbed } from 'discord.js';
-import BaseCommand from '../../utils/structures/BaseCommand';
-import DiscordClient from '../../client/client';
-import * as path from 'path';
-import * as fs from 'fs';
+import {
+  Message,
+  MessageEmbed,
+  MessageAttachment,
+  User,
+  TextChannel,
+  MessageButton,
+  MessageActionRow,
+} from "discord.js";
+import BaseCommand from "../../utils/structures/BaseCommand";
+import DiscordClient from "../../client/client";
+import * as path from "path";
+import * as fs from "fs";
+import { Command } from "reconlx";
+// import { pagination } from "reconlx";
 
 /**
  * @ArtCommand - Display art related to The Karters and send
  * as a message. -ProBoz
- * 
+ *
  * In memory of 001 Abstract Ingenuity, the i01 bot
  */
 
 export default class ArtCommand extends BaseCommand {
   constructor() {
-    super('art', 'misc', [], "Random game art.");
-  };
+    super("art", "misc", [], "Random game art.");
+  }
 
   async run(client: DiscordClient, message: Message, args: Array<string>) {
-
     const artFilePath = "/assets/gameArt/" as string; //update this string if file path changes
-    const imageDir:string = path.join(__dirname, `.${artFilePath}`)  // saving the gameArt path and correcting it
+    const imageDir: string = path.join(__dirname, `.${artFilePath}`); // saving the gameArt path and correcting it
     const artCollection = fs.readdirSync(imageDir); // reading the "images" folder and saving the file names in an array.
 
     let previousArt: Array<string> = []; //store previous shown art here
@@ -29,107 +38,215 @@ export default class ArtCommand extends BaseCommand {
     let selectorString = selector.toString() as string;
 
     while (previousArt.includes(selectorString)) {
+      selector = Math.floor(Math.random() * artCollection.length);
+    }
 
-        selector = Math.floor(Math.random() * artCollection.length);
-
-    };
-
-    if (previousArt.length > Math.floor(artCollection.length/2)) {
-
-        previousArt.shift();
-        previousArt.push(selectorString);
-
+    if (previousArt.length > Math.floor(artCollection.length / 2)) {
+      previousArt.shift();
+      previousArt.push(selectorString);
     } else {
-
-        previousArt.push(selectorString);
-
-    };
+      previousArt.push(selectorString);
+    }
 
     // console.log(`The SelectorString: ${selectorString}`); //debug selector string number
     decision = Number(selectorString) as number;
     // console.log(`The decision: ${decision}`); //debug decision number
 
     // console.log(artCollection[decision]);
-    
+
     switch (artCollection[decision]) {
       /**
        * Update this switch so that each file has its own message with embed.
        */
-      case 'BeachTrack_01.png':
+      case "BeachTrack_01.png":
         const artEmbed01 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Soak up the sun on this beach island getaway racetrack! Watch out for the crabs they like to snap away at those tires.`)
+          .setDescription(
+            `Soak up the sun on this beach island getaway racetrack! Watch out for the crabs they like to snap away at those tires.`
+          );
         message.channel.send({ embeds: [artEmbed01] });
         break;
 
-      case 'BeachTrack_02.png':
+      case "BeachTrack_02.png":
         const artEmbed02 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Soak up the sun on this beach island getaway racetrack! This is a nice curve to drift on in front of the big conch shell.`);
+          .setDescription(
+            `Soak up the sun on this beach island getaway racetrack! This is a nice curve to drift on in front of the big conch shell.`
+          );
         message.channel.send({ embeds: [artEmbed02] });
         break;
 
-      case 'BeachTrack_03.png':
+      case "BeachTrack_03.png":
         const artEmbed03 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Soak up the sun on this beach island getaway racetrack!`)
+          .setDescription(
+            `Soak up the sun on this beach island getaway racetrack!`
+          );
         message.channel.send({ embeds: [artEmbed03] });
         break;
 
-      case 'Molten Mile-min.jpg':
+      case "Molten Mile-min.jpg":
         const artEmbed04 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Be careful how you drift on these corners, you wouldn't want to fall into the lava.`)
+          .setDescription(
+            `Be careful how you drift on these corners, you wouldn't want to fall into the lava.`
+          );
         message.channel.send({ embeds: [artEmbed04] });
         break;
-      
-      case 'vulcano_art_design_concept_11_archways-min.jpg':
+
+      case "vulcano_art_design_concept_11_archways-min.jpg":
         const artEmbed05 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Molten Mile archways art concept.`)
+          .setDescription(`Molten Mile archways art concept.`);
         message.channel.send({ embeds: [artEmbed05] });
         break;
-      
-      case 'vulcano_art_enviro_concept_b_dragon-min.jpg':
+
+      case "vulcano_art_enviro_concept_b_dragon-min.jpg":
         const artEmbed06 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Live dragons on the Molten Mile!`)
+          .setDescription(`Live dragons on the Molten Mile!`);
         message.channel.send({ embeds: [artEmbed06] });
         break;
 
-      case 'vulcano_art_enviro_concept_c_gate-min.jpg':
+      case "vulcano_art_enviro_concept_c_gate-min.jpg":
         const artEmbed07 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Enter if you dare on the Molten Mile!`)
+          .setDescription(`Enter if you dare on the Molten Mile!`);
         message.channel.send({ embeds: [artEmbed07] });
         break;
 
-      case 'vulcano_art_enviro_concept_d_drop-min.jpg':
+      case "vulcano_art_enviro_concept_d_drop-min.jpg":
         const artEmbed08 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Tiki statue on the Molten Mile!`)
+          .setDescription(`Tiki statue on the Molten Mile!`);
         message.channel.send({ embeds: [artEmbed08] });
         break;
 
-      case 'vulcano_art_model_concept_12_trap_trunk-min.jpg':
+      case "vulcano_art_model_concept_12_trap_trunk-min.jpg":
         const artEmbed09 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Watch out for this trap trunk!`)
+          .setDescription(`Watch out for this trap trunk!`);
         message.channel.send({ embeds: [artEmbed09] });
         break;
 
-      case 'vulcano_art_model_concept_13_trap_axe-min.jpg':
+      case "vulcano_art_model_concept_13_trap_axe-min.jpg":
         const artEmbed10 = new MessageEmbed()
+          .setTitle("Random Art")
           .setColor("DARK_AQUA")
-          .setDescription(`Watch out for this swinging axe trap!`)
+          .setDescription(`Watch out for this swinging axe trap!`);
         message.channel.send({ embeds: [artEmbed10] });
         break;
-    
+
       default:
         break;
-    };
+    }
 
-    message.channel.send({ files: [`${__dirname}${artFilePath}${artCollection[decision]}`] });
+    message.channel.send({
+      files: [`${__dirname}${artFilePath}${artCollection[decision]}`],
+    });
 
-  };
-};
+
+
+    // Making pages out of all images in gameArt directory
+
+    const embeds: MessageEmbed[] = [];
+    const pages = {} as { [key:string]: number }  // { userId: pageNumber }
+    // const channel = {} as TextChannel;
+    // const user = {} as User;
+    const files: MessageAttachment[] = [];
+    for (let i = 0; i < artCollection.length; ++i) {
+      files.push(
+        new MessageAttachment(`${__dirname}${artFilePath}${artCollection[i]}`)
+      );
+      console.log(`${__dirname}${artFilePath}${artCollection[i]}`);
+      embeds.push(
+        new MessageEmbed()
+          .setTitle("All Arts")
+          .setDescription(`Image ${i + 1}/${artCollection.length}`)
+          .setImage(`attachment://${artCollection[i]}`)
+      );
+    }
+
+    
+
+    // creating Buttons
+    const button1 = new MessageButton()
+      .setCustomId("previousbtn")
+      .setLabel("Previous")
+      .setEmoji('◀')
+      .setStyle('SECONDARY')
+
+    const button2 = new MessageButton()
+      .setCustomId("nextbtn")
+      .setLabel("Next")
+      .setEmoji('▶')
+      .setStyle('SECONDARY')
+
+    // create an array of buttons
+    const buttonList = [button1, button2];
+
+    
+
+
+    // Create a MessageActionRow and add the button to that row.
+    const row = new MessageActionRow().addComponents(buttonList);
+
+    // sending the first embed along with the buttons
+    await message.reply({ embeds: [embeds[0]], files: [files[0]], components: [row] });
+
+
+
+
+
+
+
+
+
+    // const getRow = (id:string) => {
+    //   const row = new MessageActionRow()
+    //   row.addComponents(
+    //     new MessageButton()
+    //     .setCustomId('previousbtn')
+    //     .setStyle('SECONDARY')
+    //     .setLabel("Previous")
+    //     .setEmoji('◀')
+    //     .setDisabled(pages[id] === 0)
+    //   )
+    //   row.addComponents(
+    //     new MessageButton()
+    //     .setCustomId('nextbtn')
+    //     .setStyle('SECONDARY')
+    //     .setLabel("Next")
+    //     .setEmoji('▶')
+    //     .setDisabled(pages[id] === embeds.length - 1)
+    //   )
+
+    //   return row
+    // }
+
+
+
+
+    // message.channel.send({ embeds: embeds, files: files });
+
+    // pagination({
+    //   embeds: embeds,
+    //   channel: channel,
+    //   author: user,
+    //   time: 10000,
+    // });
+
+    // const embeds: MessageEmbed[] = [];
+    
+
+  }
+}
--- src/events/InteractionCreateEvent.ts
+++ src/events/InteractionCreateEvent.ts
@@ -1,10 +1,10 @@
 // https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-interactionCreate
-import { Interaction } from "discord.js";
+import { Interaction, MessageAttachment, MessageEmbed } from "discord.js";
 import wait from "timers/promises";
 import BaseEvent from "../utils/structures/BaseEvent";
 import DiscordClient from "../client/client";
-import { GlobalLinks } from '../reusableMiscComponents/GlobalLinks';
-import * as path from 'path';
+import * as path from "path";
+import * as fs from "fs";
 
 export default class InteractionCreateEvent extends BaseEvent {
   constructor() {
@@ -12,21 +12,78 @@ export default class InteractionCreateEvent extends BaseEvent {
   }
 
   async run(client: DiscordClient, interaction: Interaction) {
-    const memeFilePath = GlobalLinks.memesUrl as string;
-    const memeImgDir:string = path.join(__dirname, `.${memeFilePath}`);
-    console.log(memeImgDir)
 
-    if (!interaction.isSelectMenu()) return;
+    // For MemesGroup command
+    const memeFilePath = "/commands/misc/assets/memes/" as string; //update this string if file path changes
+    const memeDir: string = path.join(__dirname, `./..${memeFilePath}`); // saving the memes path and correcting it
 
-    if (interaction.customId === "memeSelect") {
-      // await interaction.update({ content: 'Something was selected!', components: [] });
-      await interaction.deferUpdate();
-      await wait.setTimeout(4000);
-      console.log(interaction.values[0]);
-      await interaction.editReply({
-        content: `You have selected ${interaction.values[0]}`,
-        // components: [],
-      });
+    if (interaction.isSelectMenu()) {
+      if (interaction.customId === "memeSelect") {
+        // Finding the path of the chosen meme from the SelectionMenu and correcting it
+        const memeChosenPath: string = path.join(
+          `${memeDir}/${interaction.values[0]}`
+        );
+        // await interaction.update({ content: `You have selected ${interaction.values[0]}`, files: [memeChosenPath], components: [] });
+
+        await interaction.deferUpdate();
+        await wait.setTimeout(4000);
+        const file = new MessageAttachment(memeChosenPath);
+        await interaction.editReply({
+          content: `You have selected ${interaction.values[0]}`,
+          files: [file],
+          // components: [],
+        });
+      }
     }
+
+    // For Art command
+    const artFilePath = "/commands/misc/assets/gameArt/" as string; //update this string if file path changes
+    const imageDir: string = path.join(__dirname, `./..${artFilePath}`); // saving the gameArt path and correcting it
+    const artCollection = fs.readdirSync(imageDir); // reading the "images" folder and saving the file names in an array.
+
+    const embeds: MessageEmbed[] = [];
+    const files: MessageAttachment[] = [];
+    for (let i = 0; i < artCollection.length; ++i) {
+      files.push(
+        new MessageAttachment(path.join(`${imageDir}${artCollection[i]}`))
+      );
+      embeds.push(
+        new MessageEmbed()
+          .setTitle("All Arts")
+          .setDescription(`Image ${i + 1}/${artCollection.length}`)
+          .setImage(`attachment://${artCollection[i]}`)
+      );
+    }
+
+    let embedNext = 0;
+    if (interaction.isButton()) {
+      if (interaction.customId === "nextbtn") {
+        ++embedNext
+        await interaction.deferUpdate();
+        await wait.setTimeout(4000);
+        await interaction.reply({ embeds: [embeds[embedNext]], files: [files[embedNext]], components: [row] });
+
+      }
+
+    }
+
+    // if (interaction.isButton()) {
+    //   const filter = (i: any) =>
+    //     i.customId === "nextbtn";
+    //   const collector = interaction.channel.createMessageComponentCollector({
+    //     filter,
+    //     time: 15000,
+    //   });
+
+    //   collector.on("collect", async (i: any) => {
+    //     if (i.customId === "nextbtn") {
+    //       await i.update({ content: "A button was clicked!", components: [] });
+    //     }
+    //   });
+
+    //   collector.on("end", (collected) =>
+    //     console.log(`Collected ${collected.size} items`)
+    //   );
+    // }
   }
 }
